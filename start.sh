c8923aafc186411a60187213eac323e11c250564
Charles Cui <alavin.cui@gmail.com>
2022-03-15 22:46:49 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rtl_fm -f 144.64M -p 15 - | direwolf -c /etc/direwolf/direwolf.conf -r 24000 -D 1 -X 1 -d xm - | ts '%Y-%m-%d %H:%M:%S' >>/var/www/html/aprs/aprs.log
+rtl_fm -f 144.64M -p 15 -s 44.1k - | direwolf -c /etc/direwolf/direwolf.conf -r 44100 -D 1 -X 1 -d xm - | ts '%Y-%m-%d %H:%M:%S' >>/var/www/html/aprs/aprs.log
